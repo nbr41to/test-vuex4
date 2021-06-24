354760d95377446c30bf66379cfb99b8d9962249
nbr41to <nbr.41to@gmail.com>
2021-06-24 20:05:46 +0900

    .

---
+++ src/components/List.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <p class="list-counter">total: {{ totalCardInList }}</p>
+      <div class="deletelist" @click="removeList">×</div>
+    </div>
+    <card v-for="(item, index) in cards" :body="item.body" :key="item.id" :cardIndex="index" :listIndex="listIndex" />
+    <card-add :listIndex="listIndex" />
+  </div>
+</template>
+
+<script>
+import CardAdd from './CardAdd';
+import Card from './Card';
+
+export default {
+  components: {
+    CardAdd,
+    Card,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    cards: {
+      type: Array,
+      required: true,
+    },
+    listIndex: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    totalCardInList() {
+      return this.cards.length;
+    },
+  },
+  methods: {
+    removeList: function () {
+      if (confirm('本当にこのリストを削除しますか？')) {
+        this.$store.dispatch('removelist', { listIndex: this.listIndex });
+      }
+    },
+  },
+};
+</script>
